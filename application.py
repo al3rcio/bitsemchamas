34585293e83332d50026eee60363f97dd664d544
Bruno Lucas <brunoplucas@gmail.com>
2021-06-11 18:42:41 -0300

    Bug fixes application.py

--- application.py
+++ application.py
@@ -1,26 +1,12 @@
 from flask import Flask, redirect, render_template, request
-from jinja2 import Environment, PackageLoader, select_autoescape
 
 # Configure application
 app = Flask(__name__)
 
-env = Environment(
-    loader=PackageLoader("application.py"),
-    autoescape=select_autoescape()
-)
-
 # Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
-# Ensure responses aren't cached
-@app.after_request
-def after_request(response):
-    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-    response.headers["Expires"] = 0
-    response.headers["Pragma"] = "no-cache"
-    return response
-
 @app.route("/")
-def indice():
+def index():
 
     return render_template('index.html')
