75a858d9c7ff82dbfe8ddd7e7e1ec897fea4b00c
Bruno Lucas <brunoplucas@gmail.com>
2021-06-16 17:19:39 -0300

    Incluído 'cave.html', 'eqt.html', 'feq.html', ajustado vídeo em 'hw.html' e incluidos links em 'application.py'

--- application.py
+++ application.py
@@ -19,3 +19,18 @@ def hw():
 
     return render_template('hw.html')
 
+@app.route("/eqt")
+def eqt():
+
+    return render_template('eqt.html')
+
+@app.route("/feq")
+def feq():
+
+    return render_template('feq.html')
+
+@app.route("/cave")
+def cave():
+
+    return render_template('cave.html')
+
