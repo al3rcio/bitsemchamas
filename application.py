ff94af8a29fa2e79806883d6c70082872e50e958
al3rcio <al3rcio@gmail.com>
2021-06-23 00:32:51 -0300

    Cores, fundo e capa em home (capa precisa de fix versão mobile)

--- application.py
+++ application.py
@@ -12,40 +12,44 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 @app.route("/")
 def index():
 
-    return render_template('index.html')
+        return render_template('index.html')
 
 @app.route("/hw")
 def hw():
 
-    return render_template('hw.html')
+        return render_template('hw.html')
+
+@app.route("/bec")
+def bec():
+        return render_template('bec.html')
 
 @app.route("/eqt")
 def eqt():
 
-    return render_template('eqt.html')
+        return render_template('eqt.html')
 
 @app.route("/feq")
 def feq():
 
-    return render_template('feq.html')
+        return render_template('feq.html')
 
 @app.route("/cave")
 def cave():
 
-    return render_template('cave.html')
+        return render_template('cave.html')
 
 @app.route("/2020")
 def doismilevinte():
 
-    return render_template('2020.html')
+        return render_template('2020.html')
 
 @app.route("/2050")
 def doismilecinquenta():
 
-    return render_template('2050.html')
+        return render_template('2050.html')
 
 @app.route("/thx")
 def thx():
 
-    return render_template('thx.html')
+        return render_template('thx.html')
 
