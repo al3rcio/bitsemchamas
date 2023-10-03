d3b88e2d5f2145217bd87c82ff532e2b4917c185
apj <al3rcio@gmail.com>
2023-10-03 08:29:01 -0300

    retomando

---
+++ app.py
@@ -0,0 +1,62 @@
+# linha pra criar o servidor
+# export FLASK_APP=app.py
+# para atualizar site: git push heroku master
+from flask import Flask, redirect, render_template, request
+
+# Configure application
+app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+@app.route("/")
+def index():
+
+        return render_template('index.html')
+
+@app.route("/hw")
+def hw():
+
+        return render_template('hw.html')
+
+@app.route("/bec")
+def bec():
+        return render_template('bec.html')
+
+@app.route("/eqt")
+def eqt():
+
+        return render_template('eqt.html')
+
+@app.route("/feq")
+def feq():
+
+        return render_template('feq.html')
+
+@app.route("/cave")
+def cave():
+
+        return render_template('cave.html')
+
+@app.route("/2020")
+def doismilevinte():
+
+        return render_template('2020.html')
+
+@app.route("/2050")
+def doismilecinquenta():
+
+        return render_template('2050.html')
+
+@app.route("/cave-game")
+def caveGame():
+        
+        return render_template('cave-game.html')
+
+@app.route("/no-mobile")
+def noMobile():
+
+        return render_template('no-mobile.html')
+
+
+
